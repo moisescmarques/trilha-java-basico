90114cb3e61bd20f42da36ce03c926f26b1e3c77
moisescmarques <moises.marques@ext.hdi.com>
2023-08-27 01:23:44 -0300

    Anat. Classes 2 - Exem de variaveis e constantes

--- JAVA-ANATOMIA-CLASSES/src/MinhaClasse.java
+++ JAVA-ANATOMIA-CLASSES/src/MinhaClasse.java
@@ -4,7 +4,13 @@ public class MinhaClasse { //utilização do Padrão CamelCase
      * Método main está sempre na classe executável
      * **/
     public static void main (String[] args){ 
-        System.out.print("Aqui estou eu fazendo o clássico 'Hello, World'  =) ");
+        //System.out.print("Aqui estou eu fazendo o clássico 'Hello, World'  =) ");
+        
+        int year = 2021; //exemplo de variavel
+        year = 2022;
+
+        final String US = "United States" //exemplo de constante. Com todas as letras maiusculas. Padrão UpperCase.
+        double medida_mm = 2.6;
     }  
 }
 
